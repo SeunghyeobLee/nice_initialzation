70d3c6bacbe75e28bbba52baf061dbfee82a442e
SeunghyeobLee <shylee@aceproject.co.kr>
2016-09-22 10:03:10 +0900

    make single instance and change size of window

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1100, height: 500, resizable: false})
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -21,6 +21,18 @@ function createWindow () {
     // when you should delete the corresponding element.
     win = null
   })
+}
+
+const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+  // Someone tried to run a second instance, we should focus our window.
+  if (win) {
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+
+if (shouldQuit) {
+  app.quit()
 }
 
 // This method will be called when Electron has finished
